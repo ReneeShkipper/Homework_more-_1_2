01b57cd5c0ae5fe6d2f6e512723dcfe432c3c917
ReneeShkipper <marinkagnome@gmail.com>
2023-11-20 09:34:15 +0300

    Add project files.

---
+++ Homework_more+_1_2/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Input number a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(next(a));
+
+static bool prime(int b)
+{
+	if (b <= 1)
+	{
+		return false;
+	}
+	for (int i = 2; i * i <= b; i++)
+	{
+		if (b % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+static int next(int a)
+{
+	int i = 1;
+    int y = 1;
+    while (prime(a+i) == false) 
+    {
+		i++;
+	}
+    while (prime(a - y) == false)
+	{
+		y++;
+	}
+	if ( i <= y)
+	{
+		return a+i;
+	}
+    return a - y;
+}
+
